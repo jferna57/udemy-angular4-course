d140679d659010275fb779bbe704a8e49acffe85
Juan Carlos Fdez <juan.carlos.fernandez@ricoh.es>
2017-06-24 15:34:41 +0200

    manage recipe ingredients. now you can see the ingredients of a recipe, and recipe now includes  the ingredients

--- prj-directives-final/src/app/recipes/recipe.service.ts
+++ prj-directives-final/src/app/recipes/recipe.service.ts
@@ -1,14 +1,29 @@
 import { EventEmitter } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
 
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-        new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+        new Recipe(
+            'Tasty Schenitzel', 
+            'A super-nasty Schenizel - just awesome', 
+            'http://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-5.jpg',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French fries', 20)
+            ]),
+        new Recipe(
+            'Big Fat Burger', 
+            'What else you need to say?', 
+            'http://aht.seriouseats.com/images/20100331-fatburger-medium.jpg',
+            [
+                new Ingredient('Buns', 2),
+                new Ingredient('Meat', 1)
+            ])
     ];
 
     getRecipes() {
