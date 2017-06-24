02151a0877c739c1de6c220ebb41008d5f2127bc
Juan Carlos Fdez <juan.carlos.fernandez@ricoh.es>
2017-06-24 15:58:51 +0200

    add the ingredients of a recipe to the shopping list. chapter 10.

--- prj-directives-final/src/app/shopping-list/shopping-list.service.ts
+++ prj-directives-final/src/app/shopping-list/shopping-list.service.ts
@@ -18,6 +18,13 @@ export class ShoppingListService {
     addIngredient(ingredient: Ingredient) {
         this.ingredients.push(ingredient);
         this.ingredientsChanged.emit(this.ingredients.slice());
+    }
 
+    addIngredients(ingredients: Ingredient[]) {
+        // for ( const ingredient of ingredients) {
+        //     this.addIngredient(ingredient);
+        // }
+        this.ingredients.push(...ingredients);
+        this.ingredientsChanged.emit(this.ingredients.slice());
     }
 }
