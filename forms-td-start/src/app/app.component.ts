05a7b88a05d8233b4b572a5bfd4cb9cfdf9bf674
Juan Carlos Fdez <juan.carlos.fernandez@ricoh.es>
2017-08-03 17:22:47 +0200

    submiting and using the form  (ch 172)

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms/src/directives';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+
+  onSubmit(form: NgForm) {
+    console.log('username: ' + form.value['username']);
+    console.log('email: ' + form.value['email']);
+    console.log('secret: ' + form.value['secret']);
+    console.log(form);
+
   }
 }
