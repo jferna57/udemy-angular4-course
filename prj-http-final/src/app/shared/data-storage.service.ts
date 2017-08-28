bc677a6717e81450913b4729ecfc2f90d7c6a50f
Juan Carlos Fdez <juan.carlos.fernandez@ricoh.es>
2017-08-28 19:23:17 +0200

    add data.json to the url

--- prj-http-final/src/app/shared/data-storage.service.ts
+++ prj-http-final/src/app/shared/data-storage.service.ts
@@ -10,11 +10,11 @@ export class DataStorageService {
   constructor(private http: Http, private recipeService: RecipeService) {}
 
   storeRecipes() {
-    return this.http.put('https://my-ng-recipe-book.firebaseio.com/', this.recipeService.getRecipes());
+    return this.http.put('https://my-ng-recipe-book.firebaseio.com/data.json', this.recipeService.getRecipes());
   }
 
   getRecipes() {
-    this.http.get('https://my-ng-recipe-book.firebaseio.com/')
+    this.http.get('https://my-ng-recipe-book.firebaseio.com/data.json')
       .map(
         (response: Response) => {
           const recipes: Recipe[] = response.json();
