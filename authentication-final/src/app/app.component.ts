850b93e7393345d05920c2a397e2415a8f2d8497
Juan Carlos Fdez <juan.carlos.fernandez@ricoh.es>
2017-08-29 21:53:40 +0200

    " should be '

--- authentication-final/src/app/app.component.ts
+++ authentication-final/src/app/app.component.ts
@@ -11,8 +11,8 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     firebase.initializeApp({
-      apiKey: "AIzaSyDGDTtGNC4FkKpSe85qb2wLHzNi2xkxFy8",
-      authDomain: "ng-recipe-book.firebaseapp.com"
+      apiKey: 'AIzaSyB5z1xBFbUa4xsv8kiaDIrxzFOJL6vPduk',
+      authDomain: 'my-ng-recipe-book.firebaseapp.com'
     });
   }
 
